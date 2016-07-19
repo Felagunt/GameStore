985abeed25f98218ddcc1a50f3b53f27f39b8350
felagunt <felagunt@live.ru>
2016-07-19 16:21:20 +0300

    add page listing

--- GameStore/Pages/Listing.aspx.cs
+++ GameStore/Pages/Listing.aspx.cs
@@ -13,10 +13,36 @@ namespace GameStore.Pages
     public partial class Listing : System.Web.UI.Page
     {
         private Repository repository = new Repository();
-        public IEnumerable<Game> GetGames()
+        private int pageSize = 4;
+
+        protected int CurrentPage
         {
-            return repository.Games;
+            get
+            {
+                int page;
+                page = int.TryParse(Request.QueryString["page"], out page) ? page : 1;
+                return page > MaxPage ? MaxPage : page;
+
+            }
         }
+
+        //возвращает наибольший номер таблицы
+        protected int MaxPage
+        {
+            get
+            {
+                return (int)Math.Ceiling((decimal)repository.Games.Count() / pageSize);
+            }
+        }
+
+        protected IEnumerable<Game> GetGames()
+        {
+            return repository.Games
+                .OrderBy(g => g.GameId)
+                .Skip((CurrentPage - 1) * pageSize)
+                .Take(pageSize);
+        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
             
